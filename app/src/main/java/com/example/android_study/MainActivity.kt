95c0d9c65bf5f17d0236aea61e87750e168ee3c0
sasasan03 <sako.hiro62@gmail.com>
2025-02-25 23:29:05 +0900

    RecyclerViewその２

--- app/src/main/java/com/example/android_study/MainActivity.kt
+++ app/src/main/java/com/example/android_study/MainActivity.kt
@@ -1,20 +1,47 @@
 package com.example.android_study
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val dataset = List(10) { "item ${it + 1}" }
+
+        val recyclerView: RecyclerView = findViewById(R.id.recycler_view111)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = MyAdapter(dataset)
+
     }
+}
+
+class MyAdapter(private val itemList: List<String>): RecyclerView.Adapter<MyAdapter.ViewHolder >() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.textView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_row, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.textView.text = itemList[position]
+    }
+
+    override fun getItemCount(): Int = itemList.size
 }
