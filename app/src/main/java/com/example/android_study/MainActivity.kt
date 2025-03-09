0fd237fc3de73d4d22fba4679d4f61753f96cc87
sasasan03 <sako.hiro62@gmail.com>
2025-03-09 17:47:06 +0900

    レイアウト作成

--- app/src/main/java/com/example/android_study/MainActivity.kt
+++ app/src/main/java/com/example/android_study/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.android_study
 
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,5 +20,22 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+}
+
+class PokemonListAdapter(private val pokeList: MutableList<Pokemon>): RecyclerView.Adapter<PokemonListAdapter.ViewHolder>(){
+
+    class ViewHolder(view:View) : RecyclerView.ViewHolder(view){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+    }
+
+    override fun getItemCount(): Int = pokeList.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
     }
 }
