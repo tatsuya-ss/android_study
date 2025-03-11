694ef2cfa22ff0a3d4b46d89f3487bb201ac3402
sasasan03 <sako.hiro62@gmail.com>
2025-03-11 23:36:46 +0900

    RetrofitのGradle導入前

--- app/src/main/java/com/example/android_study/MainActivity.kt
+++ app/src/main/java/com/example/android_study/MainActivity.kt
@@ -1,20 +1,63 @@
 package com.example.android_study
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowCompat
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: PokemonListAdapter
+    private val pokemonList = mutableListOf<PokemonResponse>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        WindowCompat.setDecorFitsSystemWindows(window, true)
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        recyclerView = findViewById(R.id.pokemonRecyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = PokemonListAdapter(pokemonList)
+        recyclerView.adapter = adapter
+
+//        lifecycleScope.launch {
+//            for (id in 1..10) {
+//                try {
+//                    val pokemon =
+//                }
+//            }
+//        }
+
     }
 }
+
+class PokemonListAdapter(private val pokemonList: MutableList<PokemonResponse>): RecyclerView.Adapter<PokemonListAdapter.ViewHolder>(){
+
+    class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        val pokeName: TextView = view.findViewById(R.id.pokeText)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val pokeText = LayoutInflater.from(parent.context)
+            .inflate(R.layout.poke_name_row, parent, false)
+        return ViewHolder(pokeText)
+    }
+
+    override fun getItemCount(): Int = pokemonList.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val pokemon = pokemonList[position]
+        holder.pokeName.text = pokemon.name
+    }
+
+}
+
+data class PokemonResponse(val name: String)
