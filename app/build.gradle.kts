ee117a535f597bdcfa7b875065d966c6a90669c2
sasasan03 <sako.hiro62@gmail.com>
2025-03-11 20:58:50 +0900

    Unknown Kotlin JVM target: 21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,11 +39,11 @@ android {
 dependencies {
 
     // RetrofitとGsonコンバーター
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation (libs.retrofit)
+    implementation (libs.converter.gson)
 
     // Kotlin Coroutines
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
     // Glide（画像読み込みライブラリ）
     implementation ("com.github.bumptech.glide:glide:4.12.0")
