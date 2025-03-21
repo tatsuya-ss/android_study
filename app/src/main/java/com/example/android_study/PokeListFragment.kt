4a5f3e6367d34729624aaef69b8eccc94d7a8192
sasasan03 <sako.hiro62@gmail.com>
2025-03-21 20:24:58 +0900

    Fragmentを追加

--- app/src/main/java/com/example/android_study/PokeListFragment.kt
+++ app/src/main/java/com/example/android_study/PokeListFragment.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.android_study.databinding.FragmentPokeListBinding
 import kotlinx.coroutines.launch
 
