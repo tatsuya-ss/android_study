e2346fcad4cf26086db29018bd9f2cdfa9c50e63
sasasan03 <sako.hiro62@gmail.com>
2025-03-27 00:32:19 +0900

    初心に戻るFragnment。これはフラグメントなのか？

---
+++ app/src/main/java/com/example/android_study/FruitFragment.kt
@@ -0,0 +1,39 @@
+package com.example.android_study
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class FruitFragment: Fragment() {
+
+    private lateinit var fruitRecyclerView: RecyclerView
+    private lateinit var adapter: FruitAdapter
+
+    private val frutisData = arrayOf("apple","banana","orange")
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_recyclerview,container,false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fruitRecyclerView = view.findViewById(R.id.fruitRecyclerView)
+        fruitRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        adapter = FruitAdapter(frutisData)
+        fruitRecyclerView.adapter = adapter
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+    }
+}
--- app/src/main/java/com/example/android_study/MainActivity.kt
+++ app/src/main/java/com/example/android_study/MainActivity.kt
@@ -1,20 +1,49 @@
 package com.example.android_study
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.frutFramelayout, FruitFragment())
+                .commit()
         }
     }
+}
+
+class FruitAdapter(private val fruits: Array<String>): RecyclerView.Adapter<FruitAdapter.ViewHolder>(){
+    class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        val frutiText: TextView = view.findViewById(R.id.fruitTextView)
+    }
+
+    //ViewHolderとレイアウトを結びつける。inflateの引数はR.layout
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.fruit_row,parent,false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = fruits.size
+
+    // ViewHolderに値をBindingする
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val fruit = fruits[position]
+        holder.frutiText.text = fruit
+    }
+
+
 }
